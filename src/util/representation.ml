19ea825f1b8e938ccb429c9bb4b665f82f9259b5
Kerem Çakırer <kcakirer@hotmail.com>
2021-03-30 02:00:21 +0300

    Add some helper functions to Representation

--- src/util/representation.ml
+++ src/util/representation.ml
@@ -10,8 +10,30 @@ let rec to_yojson (r : t) : Yojson.Safe.t = match r with
   | `Nothing -> `Null
   | `Value s -> `String s
   | `Pair (a, b) -> `List [ to_yojson a; to_yojson b ]
+  | `List [ r ] -> to_yojson r
   | `List l -> `List (BatList.map to_yojson l)
   | `Assoc l -> `Assoc (BatList.map (fun (k, v) -> k, to_yojson v) l)
   | `Tagged (n, r) -> `List [ `String n; to_yojson r ]
 
+let nothing = `Nothing
+
+let value s : t = `Value s
+
+let pair a b : t = `Pair (a, b)
+
 let list l : t = `List l
+
+let assoc l : t = `Assoc l
+
+let tagged t r : t = `Tagged (t, r)
+
+let is_value = function
+  | `Value _ -> true
+  | _ -> false
+
+let rec is_simple = function
+  | `Nothing -> true
+  | `Value _ -> true
+  | `Pair (a, b) -> is_simple a && is_simple b
+  | `List [ r ] -> is_simple r 
+  | _ -> false
